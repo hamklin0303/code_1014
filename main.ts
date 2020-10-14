ae23888a2e91e47b0e86cef00c0cec75c65fe3ce
hamklin0303 <71751313+hamklin0303@users.noreply.github.com>
2020-10-14 17:59:27 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,199 @@
-basic.forever(function () {
-	
-})
+basic.showLeds(`
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    . . . . #
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    . . . # #
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    . . # # #
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    . # # # #
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    . . . . #
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    . . . # #
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    . . # # #
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    . # # # #
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
